04403425aec4f02d728c57a5415c2166c74dc021
DAM <DAM@203-23>
2022-11-14 15:22:02 +0100

    Creació menú

--- src/conversio/Conversio.java
+++ src/conversio/Conversio.java
@@ -10,6 +10,22 @@ public class Conversio {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
+        int num;
+        
+        do{
+            System.out.println("Escull una opció: ");
+         
+            System.out.println("1. Passar de € a £");
+            System.out.println("2. Passar de £ a €");
+            System.out.println("3. Sortir");
+            
+            System.out.println("->");
+            num = sc.nextInt();
+            
+        }while(num < 3);
+        
+        
+        
         System.out.println("Introdueix en €");
         int valor_euros = sc.nextInt();
                 
