2b8cafa911a7c2f8b2c2390cc1112866cbda5f2a
DAM <DAM@203-23>
2022-11-14 15:29:31 +0100

    Conversió dels 2 valors

--- src/conversio/Conversio.java
+++ src/conversio/Conversio.java
@@ -1,38 +1,56 @@
-
 package conversio;
 
 import java.util.Scanner;
 
-
 public class Conversio {
-
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         int num;
-        
-        do{
+
+        do {
             System.out.println("Escull una opció: ");
-         
+
             System.out.println("1. Passar de € a £");
             System.out.println("2. Passar de £ a €");
             System.out.println("3. Sortir");
-            
+
             System.out.println("->");
             num = sc.nextInt();
-            
-        }while(num < 3);
-        
-        
-        
-        System.out.println("Introdueix en €");
-        int valor_euros = sc.nextInt();
-                
-        double tipus_de_canvi = 0.86d;
-        System.out.println(valor_euros + " euros € acatualment equivalen a " + (valor_euros * tipus_de_canvi) + " lliures esterlines £");
 
+            switch(num){
+            
+                case 1:
+                    System.out.println("Introdueix en €");
+                    int valor_euros = sc.nextInt();
+
+                    double tipus_de_canvi = 0.86d;
+                    System.out.println(valor_euros + " euros € acatualment equivalen a " + (valor_euros * tipus_de_canvi) + " lliures esterlines £");
+                    
+                    System.out.println("Prem RETURN per torar al MENU");
+                    sc.nextLine();
+                    sc.nextLine();
+                    break;
+                    
+                case 2:
+                    
+                    System.out.println("Introdueix en €");
+                    int valor_lliures = sc.nextInt();
+
+                    double tipus_canvi = 1.14d;
+                    System.out.println(valor_lliures + " lliures esterlines £ acatualment equivalen a " + (valor_lliures * tipus_canvi) + " euros €");
+                    
+                    System.out.println("Prem RETURN per torar al MENU");
+                    sc.nextLine();
+                    sc.nextLine();
+                    break;
+            }
+            
+            
+        }while (num < 3);
+
+        }
+    
 
     }
-    
-}
